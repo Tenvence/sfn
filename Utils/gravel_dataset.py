8eda039e8f9f70a293b8e5449758d56c922c9bff
Tenvence <cbjg395@gmail.com>
2020-07-21 11:08:15 +0800

    Add Data Augmentation

--- Utils/gravel_dataset.py
+++ Utils/gravel_dataset.py
@@ -1,7 +1,8 @@
 import os
-import shutil
+import random
 import xml.etree.cElementTree as Et
 
+import numpy as np
 import torch
 import torch.utils.data as data
 import torchvision.transforms as transforms
@@ -12,8 +13,8 @@ from Utils.iou import compute_iou
 
 class GravelDataset(data.Dataset):
     def __init__(self, anchors, image_dir_path, annotation_dir_path, data_list, input_size, scale, device, train):
-        self.crossed_image_dir_path, self.single_image_dir_path, self.crossed_image_train_dir_path, self.single_image_train_dir_path = image_dir_path
-        self.annotations_dir_path, self.annotations_train_dir_path = annotation_dir_path
+        self.crossed_image_dir_path, self.single_image_dir_path = image_dir_path
+        self.annotations_dir_path = annotation_dir_path
 
         self.input_size = input_size
         self.s_scale, self.m_scale, self.l_scale = scale
@@ -22,94 +23,46 @@ class GravelDataset(data.Dataset):
         self.l_output_size = self.input_size // self.l_scale
 
         self.dataset_list = data_list
-
         self.s_anchors, self.m_anchors, self.l_anchors = anchors
-
         self.device = device
-
         self.train = train
-
         self.iou_thresh = 0.3
-
-        if (not os.path.exists(self.crossed_image_train_dir_path)) or (not os.path.exists(self.single_image_train_dir_path)) or \
-                (not os.path.exists(self.annotations_train_dir_path)):
-            if os.path.exists(self.crossed_image_train_dir_path):
-                shutil.rmtree(self.crossed_image_train_dir_path)
-
-            if os.path.exists(self.single_image_train_dir_path):
-                shutil.rmtree(self.single_image_train_dir_path)
-
-            if os.path.exists(self.annotations_train_dir_path):
-                shutil.rmtree(self.annotations_train_dir_path)
-
-            os.makedirs(self.crossed_image_train_dir_path)
-            os.makedirs(self.single_image_train_dir_path)
-            os.makedirs(self.annotations_train_dir_path)
-
-            for p in os.listdir(self.annotations_dir_path):
-                dataset_name = p.replace('.xml', '')
-                crossed_image_path = os.path.join(self.crossed_image_dir_path, dataset_name + '.jpg')
-                single_image_path = os.path.join(self.single_image_dir_path, dataset_name + '.jpg')
-                crossed_image_train_path = os.path.join(self.crossed_image_train_dir_path, dataset_name + '.jpg')
-                single_image_train_path = os.path.join(self.single_image_train_dir_path, dataset_name + '.jpg')
-
-                annotation_path = os.path.join(self.annotations_dir_path, dataset_name + '.xml')
-                annotation_train_path = os.path.join(self.annotations_train_dir_path, dataset_name + '.xml')
-
-                crossed_image = Image.open(crossed_image_path)
-                single_image = Image.open(single_image_path)
-                gt_boxes_position = self.parse_annotation_file(annotation_path)
-                crossed_image, single_image, gt_boxes_position = self.transform_data(crossed_image, single_image, gt_boxes_position)
-
-                crossed_image.save(crossed_image_train_path)
-                single_image.save(single_image_train_path)
-
-                shutil.copyfile(annotation_path, annotation_train_path)
-
-                doc = Et.parse(annotation_train_path)
-                root = doc.getroot()
-                for i, obj in enumerate(root.findall('object')):
-                    bbox_obj = obj.find('bndbox')
-                    bbox_obj.find('xmin').text = str(gt_boxes_position[i][0].numpy())
-                    bbox_obj.find('ymin').text = str(gt_boxes_position[i][1].numpy())
-                    bbox_obj.find('xmax').text = str(gt_boxes_position[i][2].numpy())
-                    bbox_obj.find('ymax').text = str(gt_boxes_position[i][3].numpy())
-                doc.write(annotation_train_path)
 
     def __getitem__(self, index):
         dataset_name = self.dataset_list[index]
 
         crossed_image_path = os.path.join(self.crossed_image_dir_path, dataset_name + '.jpg')
         single_image_path = os.path.join(self.single_image_dir_path, dataset_name + '.jpg')
-        crossed_image_train_path = os.path.join(self.crossed_image_train_dir_path, dataset_name + '.jpg')
-        single_image_train_path = os.path.join(self.single_image_train_dir_path, dataset_name + '.jpg')
-
         annotation_path = os.path.join(self.annotations_dir_path, dataset_name + '.xml')
-        annotation_train_path = os.path.join(self.annotations_train_dir_path, dataset_name + '.xml')
 
-        crossed_image = Image.open(crossed_image_train_path)
-        single_image = Image.open(single_image_train_path)
+        crossed_image = Image.open(crossed_image_path)
+        single_image = Image.open(single_image_path)
+        bboxes = self.parse_annotation_file(annotation_path)
+        raw_w, raw_h = crossed_image.size
 
-        crossed_image = transforms.ToTensor()(crossed_image)
-        single_image = transforms.ToTensor()(single_image)
+        raw_bboxes = torch.tensor(bboxes)
 
-        if torch.cuda.is_available():
-            crossed_image, single_image = crossed_image.to(self.device), single_image.to(self.device)
+        if self.train and random.randint(0, 1) == 0:
+            crossed_image, single_image, bboxes = self.horizontal_flip(crossed_image, single_image, bboxes)
+
+        if self.train and random.randint(0, 1) == 0:
+            crossed_image, single_image, bboxes = self.vertical_flip(crossed_image, single_image, bboxes)
+
+        if self.train and random.randint(0, 1) == 0:
+            rot_degrees = np.arange(-15, 16)
+            degree = random.choice(rot_degrees)
+            crossed_image, single_image, bboxes = self.rotate(crossed_image, single_image, bboxes, degree)
+
+        crossed_image, single_image, bboxes = self.resize(crossed_image, single_image, bboxes)
+        crossed_image, single_image, bboxes = self.pad(crossed_image, single_image, bboxes)
+
+        to_tensor_trans = transforms.ToTensor()
+        crossed_image, single_image = to_tensor_trans(crossed_image), to_tensor_trans(single_image)
 
         if not self.train:
-            return crossed_image, single_image, \
-                   transforms.ToTensor()(Image.open(crossed_image_path)), \
-                   transforms.ToTensor()(Image.open(single_image_path)), \
-                   self.parse_annotation_file(annotation_path)
+            return crossed_image, single_image, raw_h, raw_w, raw_bboxes
         else:
-            gt_boxes_position = self.parse_annotation_file(annotation_train_path)  # [x_min, y_min, x_max, y_max]
-
-            s_tensor, m_tensor, l_tensor, s_coords, m_coords, l_coords = self.encode_gt_bboxes(gt_boxes_position, self.iou_thresh)
-
-            if torch.cuda.is_available():
-                s_tensor, m_tensor, l_tensor = s_tensor.to(self.device), m_tensor.to(self.device), l_tensor.to(self.device)
-                s_coords, m_coords, l_coords = s_coords.to(self.device), m_coords.to(self.device), l_coords.to(self.device)
-
+            s_tensor, m_tensor, l_tensor, s_coords, m_coords, l_coords = self.encode_gt_bboxes(bboxes, self.iou_thresh)
             return crossed_image, single_image, s_tensor, m_tensor, l_tensor, s_coords, m_coords, l_coords
 
     def __len__(self):
@@ -132,28 +85,8 @@ class GravelDataset(data.Dataset):
 
         return torch.tensor(bbox_info_list).float()
 
-    def transform_data(self, crossed_image, single_image, gt_boxes_position, is_transform=True, is_save=True, save_path=None):
-        w, h = crossed_image.size
-        scale = min(self.input_size / w, self.input_size / h)
-        resize_w, resize_h = int(w * scale), int(h * scale)
-        pad_x, pad_y = (self.input_size - resize_w) // 2, (self.input_size - resize_h) // 2
-
-        transform_image = transforms.Compose([
-            transforms.Resize((resize_h, resize_w)),
-            transforms.Pad((pad_x, pad_y, pad_x, pad_y), (125, 125, 125)),
-        ])
-
-        crossed_image = transform_image(crossed_image)
-        single_image = transform_image(single_image)
-
-        # bbox 的尺寸位置也要跟着 image 的变形而变化
-        gt_boxes_position[:, [0, 2]] = gt_boxes_position[:, [0, 2]] * scale + pad_x
-        gt_boxes_position[:, [1, 3]] = gt_boxes_position[:, [1, 3]] * scale + pad_y
-
-        return crossed_image, single_image, gt_boxes_position
-
     def encode_gt_bboxes(self, gt_boxes_position, iou_thresh):
-        max_boxes_per_scale = 150
+        max_boxes_per_scale = 30
 
         # 3 scale output tensor from Model
         s_tensor = torch.zeros((self.s_output_size, self.s_output_size, self.s_anchors.shape[0], 5))
@@ -179,6 +112,7 @@ class GravelDataset(data.Dataset):
             l_iou, l_tensor, l_coords, l_has_positive = self.compute_vector(l_tensor, l_coords, self.l_anchors, l_coord, coord, iou_thresh)
 
             # If there's no valid vector, taking out the vector of largest IoU
+            # print(s_has_positive or m_has_positive or l_has_positive)
             if not (s_has_positive or m_has_positive or l_has_positive):
                 iou_list = torch.cat([s_iou, m_iou, l_iou], dim=-1)
                 best_idx = torch.argmax(iou_list, dim=-1)
@@ -230,3 +164,87 @@ class GravelDataset(data.Dataset):
         coords[1] += 1
 
         return tensor, coords
+
+    @staticmethod
+    def horizontal_flip(crossed_img, single_img, bboxes):
+        iw, _ = crossed_img.size
+        horizontal_flip_trans = transforms.RandomHorizontalFlip(p=1)
+
+        crossed_img = horizontal_flip_trans(crossed_img)
+        single_img = horizontal_flip_trans(single_img)
+
+        bboxes[:, 0] = iw - bboxes[:, 0]
+        bboxes[:, 2] = iw - bboxes[:, 2]
+
+        return crossed_img, single_img, bboxes
+
+    @staticmethod
+    def vertical_flip(crossed_img, single_img, bboxes):
+        _, ih = crossed_img.size
+        vertical_flip_trans = transforms.RandomVerticalFlip(p=1)
+
+        crossed_img = vertical_flip_trans(crossed_img)
+        single_img = vertical_flip_trans(single_img)
+
+        bboxes[:, 1] = ih - bboxes[:, 1]
+        bboxes[:, 3] = ih - bboxes[:, 3]
+
+        return crossed_img, single_img, bboxes
+
+    @staticmethod
+    def rotate(crossed_img, single_img, bboxes, degree):
+        iw, ih = crossed_img.size
+        rotate_trans = transforms.RandomRotation(degrees=(degree, degree), expand=True, fill=128)
+
+        crossed_img = rotate_trans(crossed_img)
+        single_img = rotate_trans(single_img)
+
+        rw, rh = crossed_img.size
+
+        angle = -np.pi / 180. * degree
+        rot_mat = torch.tensor([
+            [np.cos(angle), -np.sin(angle)],
+            [np.sin(angle), np.cos(angle)]
+        ]).float()[None, None, :, :]
+
+        coords = torch.cat([
+            bboxes[:, (0, 1)][:, None, :, None],
+            bboxes[:, (2, 1)][:, None, :, None],
+            bboxes[:, (0, 3)][:, None, :, None],
+            bboxes[:, (2, 3)][:, None, :, None]
+        ], dim=1) - torch.tensor([[iw / 2], [ih / 2]])
+        rot_coords = torch.matmul(rot_mat, coords) + torch.tensor([[rw / 2], [rh / 2]])
+
+        max_coords, _ = torch.max(rot_coords, dim=1)
+        min_coords, _ = torch.min(rot_coords, dim=1)
+        max_coords = torch.squeeze(max_coords)
+        min_coords = torch.squeeze(min_coords)
+
+        bboxes = torch.cat([min_coords, max_coords], dim=1)
+        return crossed_img, single_img, bboxes
+
+    def resize(self, crossed_img, single_img, bboxes):
+        iw, ih = crossed_img.size
+        scale = self.input_size / max(iw, ih)
+        resize_trans = transforms.Resize(size=(int(ih * scale), int(iw * scale)))
+        crossed_img = resize_trans(crossed_img)
+        single_img = resize_trans(single_img)
+        bboxes *= scale
+        return crossed_img, single_img, bboxes
+
+    def pad(self, crossed_img, single_img, bboxes):
+        iw, ih = crossed_img.size
+
+        pwl = int(np.floor((self.input_size - iw) / 2))
+        pwr = int(np.ceil((self.input_size - iw) / 2))
+        pht = int(np.floor((self.input_size - ih) / 2))
+        phb = int(np.ceil((self.input_size - ih) / 2))
+
+        pad_trans = transforms.Pad(padding=(pwl, pht, pwr, phb), fill=(128, 128, 128))
+        crossed_img = pad_trans(crossed_img)
+        single_img = pad_trans(single_img)
+
+        bboxes[:, (0, 2)] += pwl
+        bboxes[:, (1, 3)] += pht
+
+        return crossed_img, single_img, bboxes
